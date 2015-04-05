dad0e113dc795e894a706f444b64153b45094e1f
Klaudiusz <klaudiusz.stawski@gmail.com>
2015-04-05 22:04:51 +0200

    Szukanie konturów za pomocą metody cannego

--- TrafficSignRecognition/Form1.Designer.cs
+++ TrafficSignRecognition/Form1.Designer.cs
@@ -28,60 +28,204 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.loadImageBtn = new System.Windows.Forms.Button();
+            this.imageBox = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.apertureLbl = new System.Windows.Forms.Label();
+            this.thresh2Lbl = new System.Windows.Forms.Label();
+            this.thresh1Lbl = new System.Windows.Forms.Label();
+            this.apertureNumeric = new System.Windows.Forms.NumericUpDown();
+            this.thresh2Numeric = new System.Windows.Forms.NumericUpDown();
+            this.thresh1Numeric = new System.Windows.Forms.NumericUpDown();
+            this.cannyBtn = new System.Windows.Forms.Button();
+            this.guassianKernelLbl = new System.Windows.Forms.Label();
+            this.guassianKernelNumeric = new System.Windows.Forms.NumericUpDown();
+            this.gaussianBtn = new System.Windows.Forms.Button();
+            this.graScaleBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.apertureNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresh2Numeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresh1Numeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guassianKernelNumeric)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // loadImageBtn
             // 
-            this.button1.Location = new System.Drawing.Point(643, 545);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(209, 36);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Open image";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.loadImageBtn.Location = new System.Drawing.Point(643, 545);
+            this.loadImageBtn.Name = "loadImageBtn";
+            this.loadImageBtn.Size = new System.Drawing.Size(209, 36);
+            this.loadImageBtn.TabIndex = 0;
+            this.loadImageBtn.Text = "Open image";
+            this.loadImageBtn.UseVisualStyleBackColor = true;
+            this.loadImageBtn.Click += new System.EventHandler(this.loadImageBtn_Click);
             // 
-            // pictureBox1
+            // imageBox
             // 
-            this.pictureBox1.Image = global::TrafficSignRecognition.Properties.Resources._2013_09_18_19_20_54;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(813, 501);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
-            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
-            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove_1);
-            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
-            this.pictureBox1.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseWheel);
+            this.imageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox.Location = new System.Drawing.Point(19, 15);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(397, 344);
+            this.imageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox.TabIndex = 1;
+            this.imageBox.TabStop = false;
+            this.imageBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.imageBox.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.imageBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove_1);
+            this.imageBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            this.imageBox.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseWheel);
             // 
             // panel1
             // 
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.apertureLbl);
+            this.panel1.Controls.Add(this.thresh2Lbl);
+            this.panel1.Controls.Add(this.thresh1Lbl);
+            this.panel1.Controls.Add(this.apertureNumeric);
+            this.panel1.Controls.Add(this.thresh2Numeric);
+            this.panel1.Controls.Add(this.thresh1Numeric);
+            this.panel1.Controls.Add(this.cannyBtn);
+            this.panel1.Controls.Add(this.guassianKernelLbl);
+            this.panel1.Controls.Add(this.guassianKernelNumeric);
+            this.panel1.Controls.Add(this.gaussianBtn);
+            this.panel1.Controls.Add(this.graScaleBtn);
+            this.panel1.Controls.Add(this.imageBox);
             this.panel1.Location = new System.Drawing.Point(33, 32);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(819, 507);
             this.panel1.TabIndex = 2;
+            // 
+            // apertureLbl
+            // 
+            this.apertureLbl.AutoSize = true;
+            this.apertureLbl.Location = new System.Drawing.Point(608, 110);
+            this.apertureLbl.Name = "apertureLbl";
+            this.apertureLbl.Size = new System.Drawing.Size(50, 13);
+            this.apertureLbl.TabIndex = 12;
+            this.apertureLbl.Text = "Aperture ";
+            // 
+            // thresh2Lbl
+            // 
+            this.thresh2Lbl.AutoSize = true;
+            this.thresh2Lbl.Location = new System.Drawing.Point(516, 110);
+            this.thresh2Lbl.Name = "thresh2Lbl";
+            this.thresh2Lbl.Size = new System.Drawing.Size(60, 13);
+            this.thresh2Lbl.TabIndex = 11;
+            this.thresh2Lbl.Text = "Threshold2";
+            // 
+            // thresh1Lbl
+            // 
+            this.thresh1Lbl.AutoSize = true;
+            this.thresh1Lbl.Location = new System.Drawing.Point(439, 110);
+            this.thresh1Lbl.Name = "thresh1Lbl";
+            this.thresh1Lbl.Size = new System.Drawing.Size(60, 13);
+            this.thresh1Lbl.TabIndex = 10;
+            this.thresh1Lbl.Text = "Threshold1";
+            // 
+            // apertureNumeric
+            // 
+            this.apertureNumeric.Location = new System.Drawing.Point(610, 137);
+            this.apertureNumeric.Name = "apertureNumeric";
+            this.apertureNumeric.Size = new System.Drawing.Size(61, 20);
+            this.apertureNumeric.TabIndex = 9;
+            // 
+            // thresh2Numeric
+            // 
+            this.thresh2Numeric.DecimalPlaces = 2;
+            this.thresh2Numeric.Location = new System.Drawing.Point(519, 137);
+            this.thresh2Numeric.Name = "thresh2Numeric";
+            this.thresh2Numeric.Size = new System.Drawing.Size(72, 20);
+            this.thresh2Numeric.TabIndex = 8;
+            // 
+            // thresh1Numeric
+            // 
+            this.thresh1Numeric.DecimalPlaces = 2;
+            this.thresh1Numeric.Location = new System.Drawing.Point(437, 137);
+            this.thresh1Numeric.Name = "thresh1Numeric";
+            this.thresh1Numeric.Size = new System.Drawing.Size(62, 20);
+            this.thresh1Numeric.TabIndex = 7;
+            // 
+            // cannyBtn
+            // 
+            this.cannyBtn.Location = new System.Drawing.Point(699, 137);
+            this.cannyBtn.Name = "cannyBtn";
+            this.cannyBtn.Size = new System.Drawing.Size(104, 23);
+            this.cannyBtn.TabIndex = 6;
+            this.cannyBtn.Text = "Canny";
+            this.cannyBtn.UseVisualStyleBackColor = true;
+            this.cannyBtn.Click += new System.EventHandler(this.cannyBtn_Click);
+            // 
+            // guassianKernelLbl
+            // 
+            this.guassianKernelLbl.AutoSize = true;
+            this.guassianKernelLbl.Location = new System.Drawing.Point(551, 62);
+            this.guassianKernelLbl.Name = "guassianKernelLbl";
+            this.guassianKernelLbl.Size = new System.Drawing.Size(40, 13);
+            this.guassianKernelLbl.TabIndex = 5;
+            this.guassianKernelLbl.Text = "Kernel:";
+            // 
+            // guassianKernelNumeric
+            // 
+            this.guassianKernelNumeric.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.guassianKernelNumeric.Location = new System.Drawing.Point(610, 60);
+            this.guassianKernelNumeric.Name = "guassianKernelNumeric";
+            this.guassianKernelNumeric.Size = new System.Drawing.Size(48, 20);
+            this.guassianKernelNumeric.TabIndex = 4;
+            // 
+            // gaussianBtn
+            // 
+            this.gaussianBtn.Location = new System.Drawing.Point(699, 57);
+            this.gaussianBtn.Name = "gaussianBtn";
+            this.gaussianBtn.Size = new System.Drawing.Size(104, 23);
+            this.gaussianBtn.TabIndex = 3;
+            this.gaussianBtn.Text = "Gaussian Filter";
+            this.gaussianBtn.UseVisualStyleBackColor = true;
+            this.gaussianBtn.Click += new System.EventHandler(this.gaussianBtn_Click);
+            // 
+            // graScaleBtn
+            // 
+            this.graScaleBtn.Location = new System.Drawing.Point(699, 15);
+            this.graScaleBtn.Name = "graScaleBtn";
+            this.graScaleBtn.Size = new System.Drawing.Size(104, 23);
+            this.graScaleBtn.TabIndex = 2;
+            this.graScaleBtn.Text = "Gray Scale";
+            this.graScaleBtn.UseVisualStyleBackColor = true;
+            this.graScaleBtn.Click += new System.EventHandler(this.graScaleBtn_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(508, 552);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(75, 23);
+            this.resetBtn.TabIndex = 3;
+            this.resetBtn.Text = "Reset Image";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(899, 593);
+            this.Controls.Add(this.resetBtn);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.loadImageBtn);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.apertureNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresh2Numeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresh1Numeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guassianKernelNumeric)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -90,9 +234,21 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button loadImageBtn;
+        private System.Windows.Forms.PictureBox imageBox;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button graScaleBtn;
+        private System.Windows.Forms.Button gaussianBtn;
+        private System.Windows.Forms.Label guassianKernelLbl;
+        private System.Windows.Forms.NumericUpDown guassianKernelNumeric;
+        private System.Windows.Forms.Button cannyBtn;
+        private System.Windows.Forms.Label apertureLbl;
+        private System.Windows.Forms.Label thresh2Lbl;
+        private System.Windows.Forms.Label thresh1Lbl;
+        private System.Windows.Forms.NumericUpDown apertureNumeric;
+        private System.Windows.Forms.NumericUpDown thresh2Numeric;
+        private System.Windows.Forms.NumericUpDown thresh1Numeric;
+        private System.Windows.Forms.Button resetBtn;
     }
 }
 
--- TrafficSignRecognition/Form1.cs
+++ TrafficSignRecognition/Form1.cs
@@ -2,6 +2,7 @@
 using Emgu.CV.Structure;
 using Emgu.CV.UI;
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,24 +14,210 @@ namespace TrafficSignRecognition
         public Form1()
         {
             InitializeComponent();
+            InitializeGuassianKernelNumeric();
+            InitializeCannyParameters();
+           
         }
 
-        private Image<Bgr, Byte> My_Image;
-
-        private void button1_Click(object sender, EventArgs e)
+        private void InitializeGuassianKernelNumeric()
         {
-            OpenFileDialog Openfile = new OpenFileDialog();
-            if (Openfile.ShowDialog() == DialogResult.OK)
+            guassianKernelNumeric.Value = guassianKernelSizeDefault;
+            guassianKernelNumeric.Maximum = 101;
+            guassianKernelNumeric.Minimum = 1;
+        }
+
+        private  void InitializeCannyParameters()
+        {
+            thresh1Numeric.Minimum = 0;
+            thresh1Numeric.Value = (decimal)thresh1ValueDefault;
+
+            thresh2Numeric.Minimum = 0;
+            thresh2Numeric.Value = (decimal)thresh2ValueDefault;
+
+            apertureNumeric.Minimum = 0;
+            apertureNumeric.Maximum = 100;
+            apertureNumeric.Value = apertureSizeDefault;
+        }
+
+        private Image<Bgr, Byte> orginalImage;
+        private Image<Bgr, Byte> convertedImage;
+        private const int guassianKernelSizeDefault = 5;
+        private const int apertureSizeDefault = 5;
+        private const double thresh1ValueDefault = 40;
+        private const double thresh2ValueDefault = 100;
+
+
+
+        private void loadImageBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog Openfile = new OpenFileDialog())
             {
-                My_Image = new Image<Bgr, byte>(Openfile.FileName);
-                pictureBox1.Image = My_Image.ToBitmap();
+                if (Openfile.ShowDialog() == DialogResult.OK)
+                {
+                    orginalImage = new Image<Bgr, byte>(Openfile.FileName);
+                    convertedImage = new Image<Bgr, byte>(orginalImage.Bitmap);
+                    imageBox.Image = convertedImage.ToBitmap();
+                }
             }
+        }
 
+        
 
-            
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            imageBox.Focus();
+        }
+        private void findBtn_Click(object sender, EventArgs e)
+        {
 
         }
 
+        private void graScaleBtn_Click(object sender, EventArgs e)
+        {
+            Image<Gray, Byte> grayImage = convertedImage.Convert<Gray,Byte>();
+            convertedImage= new Image<Bgr,byte>(grayImage.Bitmap);
+            imageBox.Image = convertedImage.Bitmap;
+        }
+
+        private void gaussianBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                convertedImage = convertedImage.SmoothGaussian((int)guassianKernelNumeric.Value);
+            }
+            catch(Emgu.CV.Util.CvException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            imageBox.Image = convertedImage.Bitmap;
+        }
+        private void cannyBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                convertedImage = new Image<Bgr, byte>(convertedImage.Canny((double)thresh1Numeric.Value, (double)thresh2Numeric.Value, (int)apertureNumeric.Value).Bitmap);
+                imageBox.Image = convertedImage.Bitmap;
+            }
+            catch (Emgu.CV.Util.CvException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            convertedImage = new Image<Bgr, byte>(orginalImage.Bitmap);
+            imageBox.Image = convertedImage.Bitmap;
+        }
+
+
+        //Find shapes from internet example
+        //private void findBtn_Click(object sender, EventArgs e)
+        //{
+        //    grayImage = orginalImage.Convert<Gray, Byte>().PyrDown().PyrUp();
+        //    convertedImgBox.Image = grayImage.ToBitmap();
+
+        //    Gray cannyThreshold = new Gray(180);
+        //    Gray cannyThresholdLinking = new Gray(120);
+        //    Gray circleAccumulatorThreshold = new Gray(120);
+
+        //    CircleF[] circles = grayImage.HoughCircles(
+        //    cannyThreshold,
+        //    circleAccumulatorThreshold,
+        //    5.0, //Resolution of the accumulator used to detect centers of the circles
+        //    10.0, //min distance 
+        //    5, //min radius
+        //    0 //max radius
+        //    )[0]; //Get the circles from the first channel
+
+
+        //    Image<Gray, Byte> cannyEdges = grayImage.Canny(cannyThreshold.Intensity, cannyThresholdLinking.Intensity);
+        //    LineSegment2D[] lines = cannyEdges.HoughLinesBinary(
+        //        1, //Distance resolution in pixel-related units
+        //        Math.PI / 45.0, //Angle resolution measured in radians.
+        //        20, //threshold
+        //        30, //min Line width
+        //        10 //gap between lines
+        //        )[0]; //Get the lines from the first channel
+
+        //    #region Find triangles and rectangles
+        //    List<Triangle2DF> triangleList = new List<Triangle2DF>();
+        //    List<MCvBox2D> boxList = new List<MCvBox2D>();
+
+        //    using (MemStorage storage = new MemStorage()) //allocate storage for contour approximation
+        //        for (Contour<Point> contours = cannyEdges.FindContours(); contours != null; contours = contours.HNext)
+        //        {
+        //            Contour<Point> currentContour = contours.ApproxPoly(contours.Perimeter * 0.05, storage);
+
+        //            if (contours.Area > 100) //only consider contours with area greater than 250
+        //            {
+        //                if (currentContour.Total == 3) //The contour has 3 vertices, it is a triangle
+        //                {
+        //                    Point[] pts = currentContour.ToArray();
+        //                    triangleList.Add(new Triangle2DF(
+        //                       pts[0],
+        //                       pts[1],
+        //                       pts[2]
+        //                       ));
+        //                }
+        //                else if (currentContour.Total == 4) //The contour has 4 vertices.
+        //                {
+        //                    #region determine if all the angles in the contour are within the range of [80, 100] degree
+        //                    bool isRectangle = true;
+        //                    Point[] pts = currentContour.ToArray();
+        //                    LineSegment2D[] edges = PointCollection.PolyLine(pts, true);
+
+        //                    for (int i = 0; i < edges.Length; i++)
+        //                    {
+        //                        double angle = Math.Abs(
+        //                           edges[(i + 1) % edges.Length].GetExteriorAngleDegree(edges[i]));
+        //                        if (angle < 80 || angle > 100)
+        //                        {
+        //                            isRectangle = false;
+        //                            break;
+        //                        }
+        //                    }
+        //                    #endregion
+
+        //                    if (isRectangle) boxList.Add(currentContour.GetMinAreaRect());
+        //                }
+        //            }
+
+        //            #region draw triangles and rectangles
+        //            Image<Bgr, Byte> triangleRectangleImage = orginalImage.CopyBlank();
+        //            foreach (Triangle2DF triangle in triangleList)
+        //                triangleRectangleImage.Draw(triangle, new Bgr(Color.DarkBlue), 2);
+        //            foreach (MCvBox2D box in boxList)
+        //                triangleRectangleImage.Draw(box, new Bgr(Color.DarkOrange), 2);
+        //            triangleRectangleImageBox.Image = triangleRectangleImage.Bitmap;
+        //            #endregion
+
+        //            #region draw circles
+        //            Image<Bgr, Byte> circleImage = orginalImage.CopyBlank();
+        //            foreach (CircleF circle in circles)
+        //                circleImage.Draw(circle, new Bgr(Color.Brown), 2);
+        //            circleImageBox.Image = circleImage.Bitmap;
+        //            #endregion
+
+        //            #region draw lines
+        //            Image<Bgr, Byte> lineImage = orginalImage.CopyBlank();
+        //            foreach (LineSegment2D line in lines)
+        //                lineImage.Draw(line, new Bgr(Color.Green), 2);
+        //            lineImageBox.Image = lineImage.Bitmap;
+        //            #endregion
+
+
+        //        }
+        //    #endregion
+
+
+
+
+        //}
+
+        #region ScaleMoveImageSection
         private bool Dragging;
         private int xPos;
         private int yPos;
@@ -51,18 +238,18 @@ namespace TrafficSignRecognition
 
         private void pictureBox1_MouseWheel(object sender, System.Windows.Forms.MouseEventArgs e)
         {
-            if (pictureBox1.Image != null)
+            if (imageBox.Image != null)
             {
-                
+
                 if (e.Delta > 0)
                 {
-                    pictureBox1.Scale(new SizeF(1.1F,1.1F));
+                    imageBox.Scale(new SizeF(1.1F, 1.1F));
                 }
                 else
                 {
-                    pictureBox1.Scale(new SizeF(0.9F, 0.9F));
+                    imageBox.Scale(new SizeF(0.9F, 0.9F));
                 }
-                
+
             }
         }
 
@@ -75,11 +262,12 @@ namespace TrafficSignRecognition
                 c.Left = e.X + c.Left - xPos;
             }
         }
+        #endregion
 
-        private void pictureBox1_MouseEnter(object sender, EventArgs e)
-        {
-            pictureBox1.Focus();
-        }
+
+
+
+
     }
 }
 
